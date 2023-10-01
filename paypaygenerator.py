d85108db9a7693732abb862f7de4ec2b1da6034a
NyaShinn1204 <134133778+NyaShinn1204@users.noreply.github.com>
2023-10-01 17:08:21 +0900

    final ez

---
+++ paypaygenerator.py
@@ -0,0 +1,41 @@
+import datetime, time, os, random, string, uuid
+from colorama import init
+from colorama import Fore, Back
+ 
+init(autoreset=True)
+zandaka = 0
+accuuid = uuid.uuid1()
+
+def randomname(n):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(n)) 
+
+def paypay_gen():
+    global zandaka
+    time.sleep(random.randint(7, 13))
+    zandaka += random.randint(4, 30)
+    print(f"{Fore.CYAN}[{datetime.datetime.now():%H:%M:%S}][success]PayPayリンクが生成されました 生成残高:"+str(zandaka)+"円")
+    print(f"{Fore.LIGHTGREEN_EX}[{datetime.datetime.now():%H:%M:%S}][info]リンクの受け取り中...")
+    time.sleep(1)
+    print(f"{Fore.CYAN}[{datetime.datetime.now():%H:%M:%S}][success]受け取りが完了しました 生成SEED:"+randomname(20)+" 受け取りアカウントUUID:"+str(accuuid))
+    print("--------------------------------------")
+    paypay_gen()
+    
+    
+
+print(f"""{Fore.LIGHTMAGENTA_EX}
+ ____             ____              ____                           _             
+|  _ \ __ _ _   _|  _ \ __ _ _   _ / ___| ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+| |_) / _` | | | | |_) / _` | | | | |  _ / _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+|  __/ (_| | |_| |  __/ (_| | |_| | |_| |  __/ | | |  __/ | | (_| | || (_) | |   
+|_|   \__,_|\__, |_|   \__,_|\__, |\____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   
+            |___/            |___/                                                                                            
+{Fore.RESET}""")
+license_key = input("Enter License-Key: ")   #PG_DEV_0wknhyJijBDn06a
+if license_key.__contains__("PG_DEV"):
+    print(f"{Fore.LIGHTGREEN_EX}[{datetime.datetime.now():%H:%M:%S}][info]HWIDを確認中...")
+    time.sleep(3)
+    print(f"{Fore.CYAN}[{datetime.datetime.now():%H:%M:%S}][success]HWIDの確認が完了し、Keyが認証されました")
+    time.sleep(2)
+    os.system('cls')
+    print(f"{Fore.LIGHTGREEN_EX}[{datetime.datetime.now():%H:%M:%S}][info]PayPayリンクをジェネレート中...")
+    paypay_gen()
